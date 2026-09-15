05014267f07a3ef40ac044d391492f03abf75cb4
Christopher12351 <tophy333333@gmail.com>
2026-09-15 13:48:09 -0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\OnpeController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(OnpeController::class)->group( function() {
+    Route::Get('grupo_votacion/{grupo_votacion}', 'grupo_votacion');
+
+});
